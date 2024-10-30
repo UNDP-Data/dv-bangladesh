fee1460e29cbbcbc87c00b5718c874c3994a2a1f
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-30 23:01:48 +0200

    Update infrastructure chart

--- src/AppInfrastructureCharts.tsx
+++ src/AppInfrastructureCharts.tsx
@@ -4,8 +4,7 @@ import '@undp-data/undp-visualization-library/dist/style.css';
 function AppInfrastructureCharts() {
   return (
     <div className='undp-container flex-div flex-wrap'>
-      {/* <MultiGraphDashboardFromConfig config='/config/configInfrastructureFull.json' /> */}
-      <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configInfrastructureFull.json' />
+      <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configInfrastructureCharts.json' />
     </div>
   );
 }
