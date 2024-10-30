8f3a5c7c7f7a87f96006c3675fcb1d35c9bc26f3
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-30 20:21:44 +0200

    Update height of the viz

--- src/AppInfrastructureFull.tsx
+++ src/AppInfrastructureFull.tsx
@@ -4,7 +4,7 @@ import '@undp-data/undp-visualization-library/dist/style.css';
 function AppInfrastructureFull() {
   return (
     <div className='undp-container flex-div flex-wrap'>
-      {/* <MultiGraphDashboardFromConfig config='/config/configInfrastructureFull_2.json' /> */}
+      {/* <MultiGraphDashboardFromConfig config='/config/configInfrastructureFull.json' /> */}
       <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configInfrastructureFull.json' />
     </div>
   );
