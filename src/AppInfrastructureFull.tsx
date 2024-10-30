b8cbf1933c38a88d5bd3167f000854e202c149e2
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-30 18:15:09 +0200

    change links

--- src/AppInfrastructureFull.tsx
+++ src/AppInfrastructureFull.tsx
@@ -4,8 +4,8 @@ import '@undp-data/undp-visualization-library/dist/style.css';
 function AppInfrastructureFull() {
   return (
     <div className='undp-container flex-div flex-wrap'>
-      <MultiGraphDashboardFromConfig config='/config/configInfrastructureFull.json' />
-      {/* <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configPopulation.json' /> */}
+      {/* <MultiGraphDashboardFromConfig config='/config/configInfrastructureFull.json' /> */}
+      <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configInfrastructureFull.json' />
     </div>
   );
 }
