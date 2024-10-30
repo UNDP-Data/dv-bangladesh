1eb783e5bcf379be3234dfdefdf8ddee76118e42
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-30 10:18:34 +0200

    Update links to data and config

--- src/AppPopulation.tsx
+++ src/AppPopulation.tsx
@@ -4,8 +4,7 @@ import '@undp-data/undp-visualization-library/dist/style.css';
 function AppPopulation() {
   return (
     <div className='undp-container flex-div flex-wrap'>
-      <MultiGraphDashboardFromConfig config='/data/configPopulation.json' />
-      <MultiGraphDashboardFromConfig config='/data/configPopulationFull.json' />
+      <MultiGraphDashboardFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configPopulation.json?token=GHSAT0AAAAAACVJJDKJMKA2NODKUOZJUEDYZZB5OIQ' />
     </div>
   );
 }
