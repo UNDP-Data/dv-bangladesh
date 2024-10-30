ae7eb7a9bec18c1584a49290eafecdc1e5978480
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-30 10:45:19 +0200

    Update data

--- src/AppCropland.tsx
+++ src/AppCropland.tsx
@@ -4,7 +4,7 @@ import '@undp-data/undp-visualization-library/dist/style.css';
 function AppCropland() {
   return (
     <div>
-      <MultiGraphDashboardWideToLongFormatFromConfig config='https://raw.githubusercontent.com/UNDP-Data/dv-bangladesh/refs/heads/main/public/config/configCropland.json' />
+      <MultiGraphDashboardWideToLongFormatFromConfig config='/config/configCropland.json' />
     </div>
   );
 }
